642911ccd6d856c53173c1f4a13582ef2969c084
Rambuk01 <festersenmario@gmail.com>
2024-11-16 21:16:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -75,13 +75,12 @@ app.layout = html.Div([
             clearable=False
         ),
     ),
+    
     html.Div([
         html.Div(
             dcc.Graph(id='cph-map'),
             style={'width':'100%','display':'inline-block','vertical-align':'top','margin':'8px 0px', 'padding':'0px'}),
         ]),
-    html.H1("Copenhagen GeoJSON Map", style={'textAlign': 'center'}),
-    dcc.Graph(id='geojson-map')
 ])
 
 """ MAP """
@@ -91,90 +90,66 @@ app.layout = html.Div([
     ],
     [
         Input(component_id='room_types', component_property='value'),
+        Input(component_id='map-type', component_property='value'),
     ]
 )
-def generate_map(room_types):
+def generate_map(room_types, map_type):
     ndata = data
     if room_types != 'all':
         ndata = ndata[ndata['room_type'] == room_types]
     
-    fig = px.scatter_mapbox(
-                        data_frame=ndata,
-                        title=f"Airbnb Listings - {room_types}" if room_types != "all" else "Airbnb Listings - All Room Types",
-                        lat="latitude",
-                        lon="longitude",
-                        #hover_name="id",
-                        hover_data=["neighbourhood_cleansed"],
-                        size="price",
-                        color="price",
-                        #size_max=15,
-                        zoom=12,
-                        height=1100,
-                        mapbox_style="open-street-map")
+    if(map_type != 'choropleth'):
+        fig = px.scatter_mapbox(
+                            data_frame=ndata,
+                            title=f"Airbnb Listings - {room_types}" if room_types != "all" else "Airbnb Listings - All Room Types",
+                            lat="latitude",
+                            lon="longitude",
+                            #hover_name="id",
+                            hover_data=["neighbourhood_cleansed"],
+                            size="price",
+                            color="price",
+                            #size_max=15,
+                            center={"lat": 55.6761, "lon": 12.5683},
+                            zoom=11,
+                            height=800,
+                            mapbox_style="open-street-map")
+        fig.update_layout(margin={"r":0,"t":0,"l":20,"b":0})
     
-    fig.update_layout(margin={"r":0,"t":0,"l":20,"b":0})
-    
-    return [fig]
+    if(map_type == 'choropleth'):
+        df_mean_prices = functions.get_mean_prices(ndata, geojson_data)
 
-
-# Callback to generate the GeoJSON map
-@app.callback(
-    Output('geojson-map', 'figure'),
-    [
-        Input('geojson-map', 'id'),  # Trigger on load
-        Input(component_id='room_types', component_property='value')
-    ]
-)
-def display_geojson(_, room_types):
-    ndata = data
-    if room_types != 'all':
-        ndata = ndata[ndata['room_type'] == room_types]
-    
-        
-
-    
-
-    df_mean_prices = functions.get_mean_prices(ndata, geojson_data)
-
-    # Create a basic map using the GeoJSON data
-    fig = px.choropleth_mapbox(
-        df_mean_prices,
-        geojson=geojson_data,
-        locations="id",             # This should match dummy_data's column
-        featureidkey="properties.bydel_nr",  # Adjust if needed
-        color="price",               # Use dummy color to fill the regions
-        mapbox_style="carto-positron",
-        hover_name="neighbourhood_cleansed",   # Show area name in the hover info
-        zoom=11,
-        center={"lat": 55.6761, "lon": 12.5683},
-        opacity=0.5,
-        height=800,
-        # color_continuous_scale=[
-        #     [0, "#edf8e9"],   # Very light green
-        #     [0.25, "#bae4b3"], # Light-medium green
-        #     [0.5, "#74c476"], # Medium green
-        #     [0.75, "#31a354"], # Dark-medium green
-        #     [1, "#006d2c"]    # Dark green
-        # ],
-        color_continuous_scale=[
-            [0, "#eff3ff"],   # Very light blue
-            [0.25, "#bdd7e7"], # Light-medium blue
-            [0.5, "#6baed6"], # Medium blue
-            [0.75, "#3182bd"], # Dark-medium blue
-            [1, "#08519c"]    # Dark blue
-        ]
-    )
-
-    # Enable scroll zoom and set margins
-    fig.update_layout(
-        margin={"r":0,"t":0,"l":0,"b":0},
-        mapbox=dict(
-            uirevision='constant',  # Keeps zoom level and position when updating
-            #scrollZoom=True         # Enables scroll zooming on the map
+        # Create a basic map using the GeoJSON data
+        fig = px.choropleth_mapbox(
+            df_mean_prices,
+            geojson=geojson_data,
+            locations="id",             # This should match dummy_data's column
+            featureidkey="properties.bydel_nr",  # Adjust if needed
+            color="price",               # Use dummy color to fill the regions
+            mapbox_style="carto-positron",
+            hover_name="neighbourhood_cleansed",   # Show area name in the hover info
+            zoom=11,
+            center={"lat": 55.6761, "lon": 12.5683},
+            opacity=0.5,
+            height=800,
+            color_continuous_scale=[
+                [0, "#eff3ff"],   # Very light blue
+                [0.25, "#bdd7e7"], # Light-medium blue
+                [0.5, "#6baed6"], # Medium blue
+                [0.75, "#3182bd"], # Dark-medium blue
+                [1, "#08519c"]    # Dark blue
+            ]
         )
-    )
-    
-    return fig
+
+        # Enable scroll zoom and set margins
+        fig.update_layout(
+            margin={"r":0,"t":0,"l":0,"b":0},
+            mapbox=dict(
+                uirevision='constant',  # Keeps zoom level and position when updating
+                #scrollZoom=True         # Enables scroll zooming on the map
+            )
+        )
+
+    return [fig]
 
 if __name__ == '__main__':
     app.run_server(debug=True, port=8893)
